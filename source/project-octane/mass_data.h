8430ebda37ad21193c8d5cf41cf73bfdc270404d
eric <p084111@gmail.com>
2020-09-04 13:09:00 -0700

    Added math, rigidbody, mass data code.

---
+++ source/project-octane/mass_data.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "math.h"
+
+namespace Octane
+{
+    struct MassData
+    {
+        //mass
+        float mass         = 1.0f;
+        float inverse_mass = 1.0f;
+
+        //center of mass
+        DirectX::XMFLOAT3 local_centroid;
+
+        //inertia
+        DirectX::XMFLOAT3X3 local_inertia;
+        DirectX::XMFLOAT3X3 local_inverse_inertia;
+    };
+}
---
+++ source/project-octane/rigidbody.cpp
@@ -0,0 +1,26 @@
+#include "rigidbody.h"
+
+namespace Octane
+{
+    RigidBody::RigidBody()
+    {
+    }
+
+    RigidBody::~RigidBody()
+    {
+    }
+
+    void RigidBody::Integrate(float dt)
+    {
+        // integrate linear velocity
+        DirectX::XMVECTOR linear_velocity       = XMLoadFloat3(&linear_velocity_);
+        DirectX::XMVECTOR force_accumulator     = XMLoadFloat3(&force_accumulator_);
+        linear_velocity                         = DirectX::XMVectorAdd(linear_velocity, DirectX::XMVectorScale(force_accumulator, mass_data_.inverse_mass * dt));
+        DirectX::XMVECTOR global_centroid       = XMLoadFloat3(&global_centroid_);
+        DirectX::XMVECTOR delta_linear_velocity = HadamardProduct(DirectX::XMVectorScale(linear_velocity, dt), linear_constraints_);
+        global_centroid                         = DirectX::XMVectorAdd(global_centroid, delta_linear_velocity);
+        force_accumulator_                      = DirectX::XMFLOAT3();
+        XMStoreFloat3(&linear_velocity_, linear_velocity);
+        XMStoreFloat3(&global_centroid_, global_centroid);
+    }
+}
