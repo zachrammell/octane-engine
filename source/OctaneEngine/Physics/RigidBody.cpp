7b4cfd6aa485cd082d94ecf81762ee40a3272a8f
Zach Rammell <zach.rammell@digipen.edu>
2020-09-17 09:32:38 -0700

    Reformatted includes/indentation

--- source/OctaneEngine/Physics/RigidBody.cpp
+++ source/OctaneEngine/Physics/RigidBody.cpp
@@ -1,32 +1,34 @@
-#include "RigidBody.h"
-#include "../Math.h"
+#include <OctaneEngine/Physics/RigidBody.h>
+#include <OctaneEngine/Math.h>
 
 namespace Octane
 {
-  void RigidBody::Integrate(float dt)
-  {
-    //integrate velocity
-    //calculate linear
-    DirectX::XMVECTOR linear_velocity = DirectX::XMVectorAdd(XMLoadFloat3(&linear_velocity_), DirectX::XMVectorScale(XMLoadFloat3(&force_accumulator_), mass_data_.inverse_mass * dt));
-    //calculate angular
-    DirectX::XMVECTOR angular_velocity = DirectX::XMVectorAdd(XMLoadFloat3(&angular_velocity_), DirectX::XMVectorScale(XMVector3Transform(XMLoadFloat3(&torque_accumulator_), XMLoadFloat3x3(&global_inverse_inertia_)), dt));
-    //store velocity result
-    XMStoreFloat3(&linear_velocity_, linear_velocity);
-    XMStoreFloat3(&angular_velocity_, angular_velocity);
-    //reset accumulator.
-    force_accumulator_  = DirectX::XMFLOAT3();
-    torque_accumulator_ = DirectX::XMFLOAT3();
-    //integrate transformation
-    //calculate linear
-    DirectX::XMVECTOR delta_linear_velocity = HadamardProduct(DirectX::XMVectorScale(linear_velocity, dt), linear_constraints_);
-    DirectX::XMVECTOR global_centroid       = DirectX::XMVectorAdd(XMLoadFloat3(&global_centroid_), delta_linear_velocity);
-    //calculate angular
-    DirectX::XMVECTOR delta_angular_velocity = HadamardProduct(DirectX::XMVectorScale(angular_velocity, dt), angular_constraints_);
-    DirectX::XMVECTOR axis                   = DirectX::XMVector3Normalize(delta_angular_velocity);
-    float             radian                 = DirectX::XMVector3Length(delta_angular_velocity).m128_f32[0] * dt;
-    DirectX::XMVECTOR orientation            = DirectX::XMQuaternionMultiply(XMLoadFloat4(&orientation_), DirectX::XMQuaternionRotationAxis(axis, radian));
-    //store transformation result
-    XMStoreFloat3(&global_centroid_, global_centroid);
-    XMStoreFloat4(&orientation_, orientation);
-  }
+
+void RigidBody::Integrate(float dt)
+{
+  //integrate velocity
+  //calculate linear
+  DirectX::XMVECTOR linear_velocity = DirectX::XMVectorAdd(XMLoadFloat3(&linear_velocity_), DirectX::XMVectorScale(XMLoadFloat3(&force_accumulator_), mass_data_.inverse_mass * dt));
+  //calculate angular
+  DirectX::XMVECTOR angular_velocity = DirectX::XMVectorAdd(XMLoadFloat3(&angular_velocity_), DirectX::XMVectorScale(XMVector3Transform(XMLoadFloat3(&torque_accumulator_), XMLoadFloat3x3(&global_inverse_inertia_)), dt));
+  //store velocity result
+  XMStoreFloat3(&linear_velocity_, linear_velocity);
+  XMStoreFloat3(&angular_velocity_, angular_velocity);
+  //reset accumulator.
+  force_accumulator_  = DirectX::XMFLOAT3();
+  torque_accumulator_ = DirectX::XMFLOAT3();
+  //integrate transformation
+  //calculate linear
+  DirectX::XMVECTOR delta_linear_velocity = HadamardProduct(DirectX::XMVectorScale(linear_velocity, dt), linear_constraints_);
+  DirectX::XMVECTOR global_centroid       = DirectX::XMVectorAdd(XMLoadFloat3(&global_centroid_), delta_linear_velocity);
+  //calculate angular
+  DirectX::XMVECTOR delta_angular_velocity = HadamardProduct(DirectX::XMVectorScale(angular_velocity, dt), angular_constraints_);
+  DirectX::XMVECTOR axis                   = DirectX::XMVector3Normalize(delta_angular_velocity);
+  float             radian                 = DirectX::XMVector3Length(delta_angular_velocity).m128_f32[0] * dt;
+  DirectX::XMVECTOR orientation            = DirectX::XMQuaternionMultiply(XMLoadFloat4(&orientation_), DirectX::XMQuaternionRotationAxis(axis, radian));
+  //store transformation result
+  XMStoreFloat3(&global_centroid_, global_centroid);
+  XMStoreFloat4(&orientation_, orientation);
+}
+
 }
--- source/OctaneEngine/Physics/RigidBody.h
+++ source/OctaneEngine/Physics/RigidBody.h
@@ -1,35 +1,38 @@
 #pragma once
 
-#include "MassData.h"
+#include <OctaneEngine/Physics/MassData.h>
+#include <DirectXMath.h>
 
 namespace Octane
 {
-  class RigidBody
-  {
-  public:
-    RigidBody()  = default;
-    ~RigidBody() = default;
 
-    void Integrate(float dt);
+class RigidBody
+{
+public:
+  RigidBody()  = default;
+  ~RigidBody() = default;
 
-  private:
-    //linear data - positional
-    DirectX::XMFLOAT3 position_;
-    DirectX::XMFLOAT3 linear_velocity_;
-    DirectX::XMFLOAT3 force_accumulator_;
-    DirectX::XMFLOAT3 linear_constraints_;
+  void Integrate(float dt);
 
-    //angular data - rotational
-    DirectX::XMFLOAT4 orientation_;
-    DirectX::XMFLOAT4 inverse_orientation_;
-    DirectX::XMFLOAT3 angular_velocity_;
-    DirectX::XMFLOAT3 torque_accumulator_;
-    DirectX::XMFLOAT3 angular_constraints_;
+private:
+  //linear data - positional
+  DirectX::XMFLOAT3 position_;
+  DirectX::XMFLOAT3 linear_velocity_;
+  DirectX::XMFLOAT3 force_accumulator_;
+  DirectX::XMFLOAT3 linear_constraints_;
 
-    //mass data
-    MassData            mass_data_;
-    DirectX::XMFLOAT3   global_centroid_;
-    DirectX::XMFLOAT3X3 global_inertia_;
-    DirectX::XMFLOAT3X3 global_inverse_inertia_;
-  };
+  //angular data - rotational
+  DirectX::XMFLOAT4 orientation_;
+  DirectX::XMFLOAT4 inverse_orientation_;
+  DirectX::XMFLOAT3 angular_velocity_;
+  DirectX::XMFLOAT3 torque_accumulator_;
+  DirectX::XMFLOAT3 angular_constraints_;
+
+  //mass data
+  MassData            mass_data_;
+  DirectX::XMFLOAT3   global_centroid_;
+  DirectX::XMFLOAT3X3 global_inertia_;
+  DirectX::XMFLOAT3X3 global_inverse_inertia_;
+};
+
 }
