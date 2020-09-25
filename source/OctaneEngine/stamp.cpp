345507cfdb9288de8f6453a6594ada7218ff209a
Zach Rammell <zach.rammell@digipen.edu>
2020-09-25 13:08:04 -0700

    Update stamp header to official DigiPen header

--- source/OctaneEngine/stamp.cpp
+++ source/OctaneEngine/stamp.cpp
@@ -1,15 +1,18 @@
-/*
-* File: stamp.cpp
-* 
-* Author:
-* 
-* Date of creation:
-* 
-* Description:
-* 
+/******************************************************************************/
+/*!
+  \par        Project Octane
+  \file       <THIS FILENAME>
+  \author     <WHO WROTE THIS>
+  \date       YYYY/MM/DD
+  \brief      <WHAT DOES IT DO>
+
+  Copyright © 2020 DigiPen, All rights reserved.
 */
+/******************************************************************************/
 
+// Main include
 
+namespace Octane
+{
 
-
-
+}
--- source/OctaneEngine/stamp.h
+++ source/OctaneEngine/stamp.h
@@ -1,15 +1,19 @@
-/*
-* File: stamp.h
-* 
-* Author:
-* 
-* Date of creation:
-* 
-* Description:
-* 
+﻿/******************************************************************************/
+/*!
+  \par        Project Octane
+  \file       <THIS FILENAME>
+  \author     <WHO WROTE THIS>
+  \date       YYYY/MM/DD
+  \brief      <WHAT DOES IT DO>
+
+  Copyright © 2020 DigiPen, All rights reserved.
 */
+/******************************************************************************/
+#pragma once
 
+// Includes
 
+namespace Octane
+{
 
-
-
+}
