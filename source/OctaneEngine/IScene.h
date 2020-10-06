09cd0b8823595f669800a7a86ba0e07ef640f0fa
Lowell Novitch <minimooch@icloud.com>
2020-10-06 15:01:57 -0700

    Renamed to Scene to IScene

--- source/OctaneEngine/IScene.h
+++ source/OctaneEngine/IScene.h
@@ -1,7 +1,7 @@
 ﻿/******************************************************************************/
 /*!
   \par        Project Octane
-  \file       Scene.h
+  \file       IScene.h
   \author     Lowell Novitch
   \date       2020/10/06
   \brief      is the class definition for a scene object
@@ -15,14 +15,14 @@
 
 namespace Octane
 {
-	class Scene
+	class IScene
 	{
         public:
 			//constructor
-          Scene() {};
+          IScene() {};
 
 		  //destructor
-		  virtual ~Scene() = default;
+		  virtual ~IScene() = default;
 
 		  virtual void Start() = 0;
 		  virtual void End() = 0;
