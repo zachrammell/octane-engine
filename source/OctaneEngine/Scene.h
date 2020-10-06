b04df835ddadcca1f2411c2f4f96f7149ca2d719
Lowell Novitch <minimooch@icloud.com>
2020-10-06 14:55:22 -0700

    added scene parent object definition

---
+++ source/OctaneEngine/Scene.h
@@ -0,0 +1,34 @@
+﻿/******************************************************************************/
+/*!
+  \par        Project Octane
+  \file       Scene.h
+  \author     Lowell Novitch
+  \date       2020/10/06
+  \brief      is the class definition for a scene object
+
+  Copyright © 2020 DigiPen, All rights reserved.
+*/
+/******************************************************************************/
+#pragma once
+
+// Includes
+
+namespace Octane
+{
+	class Scene
+	{
+        public:
+			//constructor
+          Scene() {};
+
+		  //destructor
+		  virtual ~Scene() = default;
+
+		  virtual void Start() = 0;
+		  virtual void End() = 0;
+		  virtual void Load() = 0;
+		  virtual void Unload() = 0;
+
+		  virtual void Update(float dt) = 0;
+	};
+}
