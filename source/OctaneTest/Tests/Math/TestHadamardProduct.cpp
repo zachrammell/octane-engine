6f8e5e34c41cdab2b20fa8edd5a24034fe98625e
eric <p084111@gmail.com>
2020-10-06 19:04:53 -0700

    Fix Error with namespace Math

--- source/OctaneTest/Tests/Math/TestHadamardProduct.cpp
+++ source/OctaneTest/Tests/Math/TestHadamardProduct.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Simple Hadamard Product, XMFLOAT3")
     DirectX::XMFLOAT3 a = {1.0f, 2.0f, 3.0f};
     DirectX::XMFLOAT3 b = {2.0f, 3.0f, 4.0f};
 
-    DirectX::XMFLOAT3 c = Octane::HadamardProduct(a, b);
+    DirectX::XMFLOAT3 c = Octane::Math::HadamardProduct(a, b);
 
     REQUIRE((t_float(c.x) == 2.0f));
     REQUIRE((t_float(c.y) == 6.0f));
