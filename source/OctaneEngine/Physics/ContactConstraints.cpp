2141ad5fd974ae15cc1a57a5700497eb45e0bf1e
eric <p084111@gmail.com>
2020-10-09 13:48:13 -0700

    Add a Expand Polytope Contact Point generation

--- source/OctaneEngine/Physics/ContactConstraints.cpp
+++ source/OctaneEngine/Physics/ContactConstraints.cpp
@@ -1,45 +1,117 @@
 #include <OctaneEngine/Physics/ContactConstraints.h>
-
+#include <algorithm>
 
 namespace Octane
 {
-  ContactPoint::ContactPoint()
+ContactPoint::ContactPoint()
+  : global_position_a(),
+    global_position_b(),
+    local_position_a(),
+    local_position_b(),
+    normal(),
+    r_a(),
+    r_b()
 {
 }
 
-  ContactPoint::~ContactPoint()
+ContactPoint::~ContactPoint()
 {
 }
 
-  ContactManifold::ContactManifold()
+ContactPoint& ContactPoint::operator=(const ContactPoint& rhs)
 {
-}
-
-  ContactManifold::~ContactManifold()
-{
-}
-
-  ContactConstraints::ContactConstraints()
-{
-}
-
-  ContactConstraints::~ContactConstraints()
-{
-}
-
-  void ContactConstraints::Release()
+  if (this != &rhs)
   {
+    this->collider_a = rhs.collider_a;
+    this->collider_b = rhs.collider_b;
+    this->global_position_a = rhs.global_position_a;
+    this->global_position_b = rhs.global_position_b;
+    this->local_position_a = rhs.local_position_a;
+    this->local_position_b = rhs.local_position_b;
+    this->normal = rhs.normal;
+    this->depth = rhs.depth;
+    this->normal_lambda = rhs.normal_lambda;
+    this->tangent_lambda = rhs.tangent_lambda;
+    this->bitangent_lambda = rhs.bitangent_lambda;
+    this->is_valid = rhs.is_valid;
   }
-
-  void ContactConstraints::Generate(float dt)
-  {
-  }
-
-  void ContactConstraints::Solve(float dt)
-  {
-  }
-
-  void ContactConstraints::Apply()
-  {
-  }
+  return *this;
 }
+
+bool ContactPoint::operator==(const ContactPoint& rhs) const
+{
+  return (
+    Math::IsEqualVector3(normal, rhs.normal) && depth == rhs.depth
+    && Math::IsEqualVector3(local_position_a, rhs.local_position_a)
+    && Math::IsEqualVector3(local_position_b, rhs.local_position_b));
+}
+
+void ContactPoint::Swap()
+{
+  std::swap(collider_a, collider_b);
+  std::swap(global_position_a, global_position_b);
+  std::swap(local_position_a, local_position_b);
+  normal = DirectX::XMVectorNegate(normal);
+}
+
+void ContactPoint::Clear()
+{
+  global_position_a = DirectX::XMVECTOR();
+  global_position_b = DirectX::XMVECTOR();
+  local_position_a = DirectX::XMVECTOR();
+  local_position_b = DirectX::XMVECTOR();
+  normal = DirectX::XMVECTOR();
+  depth = 0.0f;
+  normal_lambda = 0.0f;
+  tangent_lambda = 0.0f;
+  bitangent_lambda = 0.0f;
+  is_valid = true;
+}
+
+ContactPoint ContactPoint::SwappedContactPoint() const
+{
+  ContactPoint result;
+  result.normal = DirectX::XMVectorNegate(normal);
+  result.depth = this->depth;
+  result.collider_a = this->collider_b;
+  result.collider_b = this->collider_a;
+  result.global_position_a = this->global_position_b;
+  result.global_position_b = this->global_position_a;
+  result.local_position_a = this->local_position_b;
+  result.local_position_b = this->local_position_a;
+  result.is_valid = this->is_valid;
+  return result;
+}
+
+ContactManifold::ContactManifold()
+{
+}
+
+ContactManifold::~ContactManifold()
+{
+}
+
+ContactConstraints::ContactConstraints()
+{
+}
+
+ContactConstraints::~ContactConstraints()
+{
+}
+
+void ContactConstraints::Release()
+{
+}
+
+void ContactConstraints::Generate(float dt)
+{
+}
+
+void ContactConstraints::Solve(float dt)
+{
+}
+
+void ContactConstraints::Apply()
+{
+}
+} // namespace Octane
--- source/OctaneEngine/Physics/Polytope.h
+++ source/OctaneEngine/Physics/Polytope.h
@@ -1,15 +1,77 @@
 #pragma once
 
 #include <OctaneEngine/Math.h>
+#include <OctaneEngine/Physics/SupportPoint.h>
+#include <EASTL/vector.h>
 
 namespace Octane
 {
+class Simplex;
+
+class PolytopeEdge
+{
+public:
+  PolytopeEdge();
+  PolytopeEdge(size_t a, size_t b);
+  PolytopeEdge(const SupportPoint& point_a, const SupportPoint& point_b);
+  ~PolytopeEdge();
+
+public:
+  size_t a, b;
+};
+
+class Polytope;
+
+class PolytopeFace
+{
+public:
+  PolytopeFace();
+  PolytopeFace(size_t a, size_t b, size_t c, const DirectX::XMVECTOR& normal, float distance);
+  PolytopeFace(size_t a, size_t b, size_t c, const Simplex& simplex);
+  PolytopeFace(size_t a, size_t b, size_t c, const Polytope& polytope);
+  PolytopeFace(const SupportPoint& point_a, const SupportPoint& point_b, const SupportPoint& point_c);
+  ~PolytopeFace();
+
+  size_t& operator[](size_t index);
+  size_t operator[](size_t index) const;
+
+  PolytopeFace& operator=(const PolytopeFace& rhs);
+
+  bool operator==(const PolytopeFace& rhs) const;
+
+  size_t& Index(size_t index);
+  size_t Index(size_t index) const;
+
+  float TestFace(const DirectX::XMVECTOR& point) const;
+  void BarycentricCoordinates(const DirectX::XMVECTOR& point, float& u, float& v, float& w, Polytope* polytope) const;
+
+public:
+  size_t a{}, b{}, c{};
+  DirectX::XMVECTOR normal{};
+  float distance{};
+};
+
 class Polytope
 {
 public:
-  Polytope();
-  ~Polytope();
+  explicit Polytope(const Simplex& simplex);
+  ~Polytope() = default;
+
+  PolytopeFace PickClosestFace();
+  float PointFaceTest(const DirectX::XMVECTOR& point, const DirectX::XMVECTOR& normal, const DirectX::XMVECTOR& v0);
+  void Push(SupportPoint& vertex);
+  void PushFromSimplex(const SupportPoint& vertex);
+  void Expand(const SupportPoint& vertex);
+  bool IsFaceSeen(const PolytopeFace& face, const SupportPoint& vertex);
+  void AddEdge(size_t a, size_t b);
 
 private:
+  friend class NarrowPhase;
+  friend class PolytopeFace;
+
+private:
+  eastl::vector<SupportPoint> vertices;
+  eastl::vector<PolytopeEdge> edges;
+  eastl::vector<PolytopeFace> faces;
 };
 } // namespace Octane
