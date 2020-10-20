4c54adc7f1055edbd627451b391c9e007767e478
Lowell Novitch <minimooch@icloud.com>
2020-10-20 15:24:53 -0700

    created Health class

---
+++ source/OctaneEngine/Heath.cpp
@@ -0,0 +1,82 @@
+/******************************************************************************/
+/*!
+  \par        Project Octane
+  \file       Health.cpp
+  \author     Lowell Novitch
+  \date       2020/10/20
+  \brief      Class for HP for entities
+
+  Copyright © 2020 DigiPen, All rights reserved.
+*/
+/******************************************************************************/
+
+// Main include
+#include "Heath.h"
+namespace Octane
+{
+Health::Health(int maxHP) :maxHP_(maxHP), currentHP_(maxHP)
+{}
+
+int Health::GetCurrentHP() const 
+{
+  return currentHP_;
+}
+
+int Health::GetMaxHP() const
+{
+  return maxHP_;
+}
+
+bool Health::is_dead() const 
+{
+  return currentHP_ == 0;
+}
+
+//sets the value of maxHp
+//brings down currentHP if greater than newMax
+void Health::set_maxHP(int newMax) 
+{
+  maxHP_ = newMax;
+
+  if (currentHP_> newMax)
+  {
+    currentHP_ = newMax;
+  }
+}
+
+//change the currentHP by given value,
+//maxes at maxHP
+//returns true when currentHP is 0
+bool Health::ChangeCurrentHPby(int changeOfHP) 
+{
+  currentHP_ -= changeOfHP;
+
+  if (currentHP_ > maxHP_)
+  {
+    currentHP_ = maxHP_;
+  }
+
+  if (currentHP_ <= 0)
+  {
+    currentHP_ = 0;
+  }
+
+  return is_dead();
+}
+
+ //sets currentHP to given value and put in the range [0,maxHP]
+void Health::setCurrentHP(int newCurrent) 
+{
+  currentHP_ = newCurrent;
+
+  if (currentHP_ > maxHP_)
+  {
+    currentHP_ = maxHP_;
+  }
+
+  if (currentHP_ <= 0)
+  {
+    currentHP_ = 0;
+  }
+}
+}
---
+++ source/OctaneEngine/Heath.h
@@ -0,0 +1,47 @@
+﻿/******************************************************************************/
+/*!
+  \par        Project Octane
+  \file       Health.h
+  \author     Lowell Novitch
+  \date       2020/10/20
+  \brief      Class for HP for entities
+
+  Copyright © 2020 DigiPen, All rights reserved.
+*/
+/******************************************************************************/
+#pragma once
+
+// Includes
+
+namespace Octane
+{
+
+class Health
+{
+public:
+  Health(int maxHP);
+
+  int GetCurrentHP() const;
+  int GetMaxHP() const;
+
+  bool is_dead() const;
+
+  //sets the value of maxHp 
+  //brings down currentHP if greater than newMax
+  void set_maxHP(int newMax);
+
+  //change the currentHP by given value,
+  //maxes at maxHP
+  //returns true when currentHP is 0
+  bool ChangeCurrentHPby(int changeOfHP);
+
+  //sets currentHP to given value and put in the range [0,maxHP]
+  void setCurrentHP(int newCurrent);
+
+private:
+  int maxHP_;
+  int currentHP_;
+
+
+};
+}
