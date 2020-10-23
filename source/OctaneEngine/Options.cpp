d447f3fc8b77e999f71f6c913c24945c944c8b18
Jiwon Jung <jjwon6218@gmail.com>
2020-10-23 13:43:39 -0700

    Commented out part where cause error

--- source/OctaneEngine/Options.cpp
+++ source/OctaneEngine/Options.cpp
@@ -1,31 +1,31 @@
-#include <OctaneEngine/Options.h>
-#include <imgui.h>
-#include <OctaneEngine/InputHandler.h>
-#include <OctaneEngine/WindowManager.h>
-
-namespace Octane
-{
-void Octane::Options::OptionMenu()
-{
-  ImGuiWindowFlags const flags
-    = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize;
-  int const w = Get<WindowManager>()->GetWidth();
-  int const h = Get<WindowManager>()->GetHeight();
-  if (ImGui::Begin("Option Menu", NULL, flags))
-  {
-
-    ImGui::SetWindowPos(ImVec2(0.5f * w, 0.5f * h), ImGuiCond_Always);
-
-    if (ImGui::Button("FullScreen"))
-    {
-
-    }
-    //if (ImGui::Button("Quit")) //|| Get<InputHandler>()->KeyReleased(SDLK_ESCAPE))
-    //{
-
-    //}
-  }
-  ImGui::End();
-}
-
-} // namespace Octane
+//#include <OctaneEngine/Options.h>
+//#include <imgui.h>
+//#include <OctaneEngine/InputHandler.h>
+//#include <OctaneEngine/WindowManager.h>
+//
+//namespace Octane
+//{
+//void Octane::Options::OptionMenu()
+//{
+//  ImGuiWindowFlags const flags
+//    = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize;
+//  int const w = Get<WindowManager>()->GetWidth();
+//  int const h = Get<WindowManager>()->GetHeight();
+//  if (ImGui::Begin("Option Menu", NULL, flags))
+//  {
+//
+//    ImGui::SetWindowPos(ImVec2(0.5f * w, 0.5f * h), ImGuiCond_Always);
+//
+//    if (ImGui::Button("FullScreen"))
+//    {
+//
+//    }
+//    //if (ImGui::Button("Quit")) //|| Get<InputHandler>()->KeyReleased(SDLK_ESCAPE))
+//    //{
+//
+//    //}
+//  }
+//  ImGui::End();
+//}
+//
+//} // namespace Octane
--- source/OctaneEngine/Options.h
+++ source/OctaneEngine/Options.h
@@ -1,33 +1,33 @@
-/******************************************************************************/
-/*!
-  \par        Project Octane
-  \file       Options.h
-  \author     Jiwon Jung
-  \date       2020/10/23
-  \brief      Class for Option menu
-
-  Copyright © 2020 DigiPen, All rights reserved.
-*/
-/******************************************************************************/
-#pragma once
-
-// Includes
-#include <OctaneEngine/SceneSys.h>
-
-namespace Octane
-{
-
-//forward ref
-class InputHandler;
-
-class Options final : public IScene
-{
-public:
-  Options();
-
-  void OptionMenu();
-
-private:
-
-};
-} // namespace Octane
+///******************************************************************************/
+///*!
+//  \par        Project Octane
+//  \file       Options.h
+//  \author     Jiwon Jung
+//  \date       2020/10/23
+//  \brief      Class for Option menu
+//
+//  Copyright © 2020 DigiPen, All rights reserved.
+//*/
+///******************************************************************************/
+//#pragma once
+//
+//// Includes
+//#include <OctaneEngine/SceneSys.h>
+//
+//namespace Octane
+//{
+//
+////forward ref
+//class InputHandler;
+//
+//class Options final : public IScene
+//{
+//public:
+//  Options();
+//
+//  void OptionMenu();
+//
+//private:
+//
+//};
+//} // namespace Octane
