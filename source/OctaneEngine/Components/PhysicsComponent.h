6364b27c546d6655fa553f98495c4a96f9018674
Dante Chiesa <d.chiesa@digipen.edu>
2020-12-04 12:30:06 -0800

    Make SetPosition private as it shouldn't be used

--- source/OctaneEngine/Components/PhysicsComponent.h
+++ source/OctaneEngine/Components/PhysicsComponent.h
@@ -8,17 +8,19 @@ namespace Octane
 class PhysicsComponent
 {
 public:
-  void SetPosition(const DirectX::XMFLOAT3& position) const;
   void SetRotation(const DirectX::XMFLOAT4& rotation) const;
   void SetRotation(const DirectX::XMFLOAT3& euler_angle) const;
   void ApplyForce(const DirectX::XMFLOAT3& force) const;
   void ApplyTorque(const DirectX::XMFLOAT3& torque) const;
   void SetGravity(float force) const;
 
-public:
   friend class PhysicsSys;
   friend class ComponentSys;
   btRigidBody* rigid_body = nullptr;
   EntityID parent_entity = INVALID_ENTITY;
+
+private:
+  // position should only be set through transform
+  void SetPosition(const DirectX::XMFLOAT3& position) const;
 };
 } // namespace Octane
